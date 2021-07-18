9ab721b5941e6047974b98e20a99625e47991970
Emerson Yllisca <veraraul495@gmail.com>
2021-07-18 18:46:56 -0500

    database y config

---
+++ api/config/server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('../../src/routes/index.routes');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api-inventario', routes)
+
+module.exports = app;
