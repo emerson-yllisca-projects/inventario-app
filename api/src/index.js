f576ae1c0ce6e89cd268841884fb77175cd8c90b
Emerson Yllisca <veraraul495@gmail.com>
2021-07-28 21:28:21 -0500

    docker test

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,10 @@
- 
+require('dotenv').config();
+const app = require('../config/server/app');
+const port = process.env.PORT || 4001;
+
+const logger = require('../config/server/logger')
+
+app.listen(port , () => {
+    logger.info("Iniciando");
+    console.log('Servidor inicializado en el puerto' + port );
+})
